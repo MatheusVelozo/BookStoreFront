a66c3e1431409b7a93ad35c7a2465919bd34a8d4
MatheusVelozo <matheusvelozo@hotmail.com>
2023-11-29 18:24:54 -0300

    Implantando Categoria Create/mensagens de erro.

--- src/app/components/views/categoria/categoria.service.ts
+++ src/app/components/views/categoria/categoria.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs'; 
 import { Categoria } from './categoria.model';
 import { environment } from '../../../../environments/environments';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +13,24 @@ export class CategoriaService {
 
   apiUrl: string = environment.apiUrl;
 
-  constructor(private Http: HttpClient) { }
+  constructor(private Http: HttpClient, private _snack: MatSnackBar) { }
 
   findAll() : Observable<Categoria[]> {
     const url = `${this.apiUrl}/categorias`
     return this.Http.get<Categoria[]>(url)
 
+  }
+
+  create(categoria: Categoria): Observable<Categoria>{
+    const url =`${this.apiUrl}/categorias`
+    return this.Http.post<Categoria>(url, categoria);
+  }
+
+  mensagem(str: String): void {
+    this._snack.open(`${str}`, 'OK', {
+    horizontalPosition: 'end',
+    verticalPosition: 'top',
+    duration: 3000
+    })
   }
 }
